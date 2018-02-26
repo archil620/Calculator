53112a5733e01aeeeb2137b8e55dd7c2af544d63
archil620 <ar898479@dal.ca>
2018-02-25 20:50:13 -0400

    Final code optimization and Minor UI changes has been done.

--- app/src/main/java/com/example/archil/calculator/MainActivity.java
+++ app/src/main/java/com/example/archil/calculator/MainActivity.java
@@ -5,6 +5,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+
+//Instead of making onclicklistner for single buttons we can implement it on main activity so that we don't have to write it every time, it will need void method of inClick.
  public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
 
             private Button seven, eight, nine, plus, minus, equal, div, mul, clear, dot, one, two, three, four, five, six, sqrt, allclear, zero,plusminus;
@@ -18,53 +20,63 @@ import android.widget.EditText;
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_main);
 
-                plusminus= findViewById(R.id.plusminus);
-                allclear = findViewById(R.id.allclear);
-                sqrt = findViewById(R.id.sqrt);
-                editText = findViewById(R.id.editText);
+                //To find button in the layout
+
+                zero = findViewById(R.id.zero);
+                one = findViewById(R.id.one);
+                two = findViewById(R.id.two);
+                three = findViewById(R.id.three);
+                four = findViewById(R.id.four);
+                five = findViewById(R.id.five);
+                six = findViewById(R.id.six);
                 seven = findViewById(R.id.seven);
                 eight = findViewById(R.id.eight);
                 nine = findViewById(R.id.nine);
-                six = findViewById(R.id.six);
-                five = findViewById(R.id.five);
-                four = findViewById(R.id.four);
-                three = findViewById(R.id.three);
-                two = findViewById(R.id.two);
-                one = findViewById(R.id.one);
-                zero = findViewById(R.id.zero);
+                dot = findViewById(R.id.dot);
+
+                allclear = findViewById(R.id.allclear);
+                editText = findViewById(R.id.editText);
+                clear = findViewById(R.id.clear);
+
+                plusminus= findViewById(R.id.plusminus);
+                sqrt = findViewById(R.id.sqrt);
                 plus = findViewById(R.id.plus);
                 minus = findViewById(R.id.minus);
                 equal = findViewById(R.id.equals);
                 div = findViewById(R.id.divide);
                 mul = findViewById(R.id.multiply);
-                clear = findViewById(R.id.clear);
-                dot = findViewById(R.id.dot);
+
+               //Initial screen declaration
                 editText.setText("");
 
-                plusminus.setOnClickListener(this);
+                //Implementing onclicklisteners method to all buttons.
                 allclear.setOnClickListener(this);
-                sqrt.setOnClickListener(this);
+                editText.setOnClickListener(this);
+                clear.setOnClickListener(this);
+
+                zero.setOnClickListener(this);
+                one.setOnClickListener(this);
+                two.setOnClickListener(this);
+                three.setOnClickListener(this);
+                four.setOnClickListener(this);
+                five.setOnClickListener(this);
+                six.setOnClickListener(this);
                 seven.setOnClickListener(this);
                 eight.setOnClickListener(this);
                 nine.setOnClickListener(this);
-                seven.setOnClickListener(this);
-                six.setOnClickListener(this);
-                five.setOnClickListener(this);
-                four.setOnClickListener(this);
-                three.setOnClickListener(this);
-                two.setOnClickListener(this);
-                one.setOnClickListener(this);
-                zero.setOnClickListener(this);
+                dot.setOnClickListener(this);
+
                 plus.setOnClickListener(this);
                 minus.setOnClickListener(this);
-                equal.setOnClickListener(this);
-                clear.setOnClickListener(this);
                 mul.setOnClickListener(this);
                 div.setOnClickListener(this);
-                dot.setOnClickListener(this);
-                editText.setOnClickListener(this);
+                equal.setOnClickListener(this);
+                plusminus.setOnClickListener(this);
+                sqrt.setOnClickListener(this);
+
             }
 
+            //this method will call onClick method to perform actions.
             private void setNumberBefore(Character sign) {
                 String numBef = editText.getText().toString();
                 if (numBef.startsWith(".")) {
@@ -76,6 +88,7 @@ import android.widget.EditText;
                 editText.setText("0");
             }
 
+           //for a default screen.
             private void getKeyboard(String string) {
 
                 String currentString = editText.getText().toString();
@@ -83,7 +96,7 @@ import android.widget.EditText;
                 if (currentString.equals("0")) {
                     currentString = "";
                 }
-
+            // For preventing users from entering multiple dots(.)
                 if (currentString.contains(".")) {
                     if (string.contains(".")) {
                         currentString = "0";
@@ -91,24 +104,23 @@ import android.widget.EditText;
                         currentString = "0";
                     }
                 }
-
                 currentString += string;
-
                 if (currentString.contains("0.")) {
                     editText.setText(currentString);
                 }
                 editText.setText(currentString);
-
             }
 
-            private void setResult() {
 
+            private void setResult() {
+               //Users to enter second number
                 if (!Float.isNaN(numberBefore)) {
                     String numberAft = editText.getText().toString();
 
                     float numberAfter = Float.parseFloat(numberAft);
                     float finalResult = 0;
 
+                    //Onclick method will perform this operations
                     if (signOperator == '+') {
                         finalResult = numberAfter + numberBefore;
                     }
@@ -128,22 +140,23 @@ import android.widget.EditText;
                     if (signOperator == '√') {
                         finalResult = (float)Math.sqrt(numberBefore);
                     }
-
+                     //If number is greter than zero then it will minus it and if it is negative it will make it positive
                     if (signOperator == '±' ) {
                         numberAfter = Float.parseFloat(editText.getText().toString());
 
                         if(numberAfter >= 0)
                             finalResult = numberBefore * (-1);
                         else
-                            finalResult = numberBefore;
+                            finalResult = numberBefore * (+1);
                     }
 
+                    //If user wants to perform one more equation, for eg after 3+2 if he wants +5 this  method will be called.
                     if (signOperator =='='){
                         numberAfter = Float.parseFloat(editText.getText().toString());
                         switch (pro){
                             case "+":
                                 finalResult = numberBefore +numberAfter;
-                                editText.setText(finalResult+"");
+                                editText.setText(finalResult+ "");
                                 break;
                             case "-":
                                 finalResult = numberBefore - numberAfter;
@@ -169,6 +182,8 @@ import android.widget.EditText;
                                     finalResult = numberAfter;
                         }
                     }
+
+                    //to remove NaN and Infinite error message.
                     if (Float.isInfinite(finalResult) || Float.isNaN(finalResult)) {
                         editText.setText("0");
                     } else {
@@ -178,6 +193,7 @@ import android.widget.EditText;
                 }
             }
 
+            //Getting id of the operators to perform action
             @Override
             public void onClick(View v) {
 
@@ -222,8 +238,6 @@ import android.widget.EditText;
                         pro="±";
                         break;
 
-
-
                     case R.id.clear:
                         if(!editText.getText().toString().equals("")) {
                             String value = editText.getText().toString();
@@ -234,12 +248,11 @@ import android.widget.EditText;
                         }
                         break;
 
-
                     default:
                         String defaultSign = ((Button) v).getText().toString();
                         getKeyboard(defaultSign);
                         break;
                 }
             }
-        }
+}
 
